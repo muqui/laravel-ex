cfa3736088ddc875068044548a4b7f35f1ea4163
AlbertoCorona <muqui@hotmail.com>
2019-02-15 17:34:23 -0600

    index front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,56 @@
 <html>
     <head>
         <title>Laravel</title>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <script type="text/javascript" src="{{ URL::asset('js/jquery.js') }}"></script> 
+        <script type="text/javascript" src="{{ URL::asset('js/popper.min.js') }}"></script> 
+        <script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+       <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+      width: 100%;
+      height: 100%;
+  }
+  </style>
+       
+        
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Tienda de video juegos</div>
-            </div>
-        </div>
+       
+            
+       <div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ URL::asset('image/red-dead-redemption-2.jpg') }}" alt="red dead redemption 2" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ URL::asset('image/fifa-19.jpg') }}" alt="FIFA 19" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ URL::asset('image/Resident-Evil-2.jpg') }}" alt="Resident Evil 2" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+            
+       
     </body>
 </html>
