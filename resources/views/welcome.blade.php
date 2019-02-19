b3059ac8e0e2d0ca7b5bf7fe02acd585c475f865
AlbertoCorona <muqui@hotmail.com>
2019-02-19 12:08:45 -0600

    index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,33 +33,33 @@
 </div>
 <div class="container-fluid text-center">
   <div class ="row">
-    <div class ="col-md-2 col-sm-6">
+    <div class ="col-6 col-md-2 col-sm-4">
        <a href="#" class="img-fluid">
           <img src="{{ URL::asset('image/1.jpg') }}" class="img-fluid" alt="">
         </a>
     </div>
-    <div class ="col-md-2 col-sm-6">
+    <div class ="col-6 col-md-2 col-sm-4">
        <a href="#" class="img-fluid">
           <img src="{{ URL::asset('image/2.jpg') }}"  class="img-fluid" alt="">
         </a>
     </div>  
-    <div class ="col-md-2 col-sm-6">
+    <div class ="col-6 col-md-2 col-sm-4">
        <a href="#" class="img-fluid">
           <img src="{{ URL::asset('image/3.jpg') }}" class="img-fluid" alt="">
         </a>
     </div>  
   
-    <div class ="col-md-2 col-sm-6">
+    <div class ="col-6 col-md-2 col-sm-4">
       <a href="#" class="img-fluid">
           <img src="{{ URL::asset('image/5.jpg') }}" class="img-fluid" alt="">
         </a>
     </div >
-    <div class ="col-md-2 col-sm-6">
+    <div class ="col-6 col-md-2 col-sm-4">
        <a href="#" class="img-fluid">
           <img src="{{ URL::asset('image/6.jpg') }}" class="img-fluid" alt="">
         </a>
     </div>
-    <div class ="col-md-2 col-sm-6">
+    <div class ="col-6 col-md-2 col-sm-4">
        <a href="#" class="img-fluid">
           <img src="{{ URL::asset('image/6.jpg') }}" class="img-fluid" alt="">
         </a>
